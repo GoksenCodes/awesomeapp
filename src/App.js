6ca43a32517309168ce7d8ab050b51926530d081
Goksen <goksengorgulu@gmail.com>
2020-02-24 11:55:19 +0100

    initialized the app

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <p>It's exciting to learn React</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -16,9 +16,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello React
         </a>
-      </header>
     </div>
   );
 }
